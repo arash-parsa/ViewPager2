0ffd86723961b38b88f79efcc023d5e2d79deff4
Arash <arashparsa.work@gmail.com>
2020-04-15 13:15:26 +0430

    ViewPager+TabLayout

---
+++ app/src/main/java/com/example/viewpager2/FragmentSlide.java
@@ -0,0 +1,36 @@
+package com.example.viewpager2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSlide extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slide, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView = view.findViewById(R.id.tv_slide_charachter);
+        textView.setText(getArguments().getString("data"));
+
+    }
+
+    public static FragmentSlide newInstance(String data) {
+
+        Bundle args = new Bundle();
+        args.putString("data", data);
+        FragmentSlide fragment = new FragmentSlide();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
---
+++ app/src/main/java/com/example/viewpager2/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.viewpager2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager2 = findViewById(R.id.vp_main);
+        SlideAdapter slideAdapter = new SlideAdapter(this);
+        viewPager2.setAdapter(slideAdapter);
+        viewPager2.setOrientation(viewPager2.ORIENTATION_VERTICAL);
+        viewPager2.setCurrentItem(3);
+
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout_main);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("A");
+                        break;
+                    case 1:
+                        tab.setText("B");
+                        break;
+                    case 2:
+                        tab.setText("C");
+                        break;
+                    case 3:
+                        tab.setText("D");
+                        break;
+                    case 4:
+                        tab.setText("E");
+                        break;
+                    default:
+                        tab.setText("A");
+                        break;
+                }
+            }
+        });
+        tabLayoutMediator.attach();
+
+    }
+}
